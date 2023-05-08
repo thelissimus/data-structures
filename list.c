5eda8ab5b28ef50d4d34d92e6b948ff3c34f10a5
the lissimus <thelissimus@tuta.io>
2023-05-08 23:33:17 +0500

    impl: `decons` recursive `rforeach` and `rmap`
    FIXED:
    * Callback function arguments
    
    DELETED:
    * Unnecessary `head` and `tail`

--- list.c
+++ list.c
@@ -6,12 +6,12 @@ typedef struct List_ {
 } List;
 
 List *cons(int, List *);
+void decons(List *);
 
-int *head(List *);
-List *tail(List *);
-
-void foreach(List *, void (*)(List *));
-List *map(List *, List *(*)(List *));
+void foreach(List *, void (*)(int));
+List *map(List *, int (*)(int));
+void rforeach(List *, void (*)(int));
+List *rmap(List *, int (*)(int));
 
 List *
 cons(int head, List *tail)
@@ -23,33 +23,46 @@ cons(int head, List *tail)
     return l;
 }
 
-int *
-head(List *l)
-{
-	if (l == NULL) return NULL;
-	return &l->head;
-}
-
-List *
-tail(List *l)
-{
-	if (l == NULL) return NULL;
-	return l->tail;
-}
-
 void
-foreach(List *l, void (*fn)(List *))
+decons(List *l)
 {
 	List *curr, *next;
 	for (curr = l; curr != NULL; curr = next) {
 		next = curr->tail;
-		fn(curr);
+		free(curr);
+	}
+}
+
+void
+foreach(List *l, void (*fn)(int))
+{
+	List *curr;
+	for (curr = l; curr != NULL; curr = curr->tail) {
+		fn(curr->head);
 	}
 }
 
 List *
-map(List *l, List *(*fn)(List *))
+map(List *l, int (*fn)(int))
 {
-	if (l == NULL) return l;
-	return cons(*head(fn(l)), map(tail(l), fn));
+	List *curr, *acc;
+	for (curr = l, acc = NULL; curr != NULL; curr = curr->tail) {
+		acc = cons(fn(curr->head), acc);
+	}
+	return acc;
+}
+
+void
+rforeach(List *l, void (*fn)(int))
+{
+	if (l == NULL) return;
+	fn(l->head);
+	rforeach(l->tail, fn);
+}
+
+List *
+rmap(List *l, int (*fn)(int))
+{
+	if (l == NULL) return NULL;
+	return cons(fn(l->head), rmap(l->tail, fn));
 }
