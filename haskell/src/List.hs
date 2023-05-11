3f592b766f91d193526c3924acd8c75e068e5500
the lissimus <thelissimus@tuta.io>
2023-05-11 21:32:46 +0500

    impl: Haskell `List`
    IMPL:
    List instances for
    * Semigroup
    * Monoid
    * Foldable
    * Functor
    * Applicative
    * Monad

--- haskell/src/List.hs
+++ haskell/src/List.hs
@@ -1,1 +1,41 @@
+{-# LANGUAGE InstanceSigs #-}
+
 module List where
+
+data List a
+  = Cons a (List a)
+  | Nil
+  deriving (Show)
+
+instance Semigroup (List a) where
+  (<>) :: List a -> List a -> List a
+  Nil <> b = b
+  (Cons h t) <> b = Cons h (t <> b)
+
+instance Monoid (List a) where
+  mempty :: List a
+  mempty = Nil
+
+instance Foldable List where
+  foldr :: (a -> b -> b) -> b -> List a -> b
+  foldr _ z Nil = z
+  foldr f z (Cons h t) = f h (foldr f z t)
+
+instance Functor List where
+  fmap :: (a -> b) -> List a -> List b
+  fmap _ Nil = Nil
+  fmap f (Cons h t) = Cons (f h) (fmap f t)
+
+instance Applicative List where
+  pure :: a -> List a
+  pure a = Cons a Nil
+
+  (<*>) :: List (a -> b) -> List a -> List b
+  fs <*> xs = foldMap (`fmap` xs) fs
+
+instance Monad List where
+  return :: a -> List a
+  return = pure
+
+  (>>=) :: List a -> (a -> List b) -> List b
+  xs >>= f = foldMap f xs
