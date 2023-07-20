8bdc2bf2e22f604c4f005e4e9cece89b8c5694e4
thelissimus <thelissimus@tuta.io>
2023-07-20 10:25:49 +0500

    impl[hs]: Foldable Tree

--- haskell/src/Tree.hs
+++ haskell/src/Tree.hs
@@ -11,3 +11,8 @@ instance Functor Tree where
   fmap :: (a -> b) -> Tree a -> Tree b
   fmap _ Leaf = Leaf
   fmap f (Node a l r) = Node (f a) (fmap f l) (fmap f r)
+
+instance Foldable Tree where
+  foldr :: (a -> b -> b) -> b -> Tree a -> b
+  foldr _ z Leaf = z
+  foldr f z (Node a l r) = foldr f (f a (foldr f z r)) l
